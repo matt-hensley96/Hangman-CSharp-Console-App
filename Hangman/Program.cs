1a38f0ec2fc134781076ee4a88407e957adc990f
Matt <matthensley42@gmail.com>
2019-12-20 10:21:59 +0000

    completed first draft without a UI

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -5,31 +5,91 @@ namespace Hangman
     class Program
     {
         static void Main(string[] args)
+        // TO DO: Add an ASCI UI as suggested in the email
         {
             Presenter.Greeting();
 
             // Initialise anything needed later
             int livesRemaining = 9;
+            bool playerWon = false;
 
             // Store the names of the two players
             Console.WriteLine("Player 1, enter your name, then hit Enter.");
             string playerOneName = Console.ReadLine();
+
+            Console.WriteLine();
             Console.WriteLine("Player 2, enter your name, then hit Enter.");
             string playerTwoName = Console.ReadLine();
 
-            // Stores the secret word
-            Console.WriteLine($"{playerOneName}, type the secret word, then hit Enter.");
-            string secretWord = Console.ReadLine();
-            // TO DO: Validate that the secret word doesn't have more unique letters than player 2's starting number of lives (9).
-            // TO DO: Obscure the letters in the secret word (as the user types them) using * characters (like a password)
 
-            // TO DO: Player 2 keeps guessing letters from player 1's word
-            Console.WriteLine($"{playerTwoName}, you have {livesRemaining} lives left. Guess a letter from the secret word, then hit enter.");
-            string guessOne = Console.ReadLine();
-            //      TO DO: If they get a letter correct, every instance of this letter (and previously guessed letters) is displayed in its position, e.g R*E*E**
-            //      TO DO: If they get a letter wrong, they lose a life
-            //      TO DO: Once they have zero lives, it's game over and the full word gets revealed
-            // TO DO: Add an ASCI UI as suggested in the email
+
+
+            // Store the secret word
+            Console.WriteLine();
+            Console.WriteLine($"{playerOneName}, type a secret word, then hit Enter. It must be one word, containing letters only (less than 10).");
+            string secretWordInput = Console.ReadLine().ToUpper();
+            // TO DO: Obscure the letters in the secret word (as the user types them) using * characters (like a password)
+            string secretWord = Validator.GetValidSecretWord(secretWordInput);
+            // Create an array of dashes, the length of the secret word
+            char[] answerPreview = new char[secretWord.Length];
+            for (int i = 0; i < answerPreview.Length; i++)
+            {
+                answerPreview[i] = '_';
+            }
+
+
+
+            // Main Gameplay:
+            while (livesRemaining > 0 && !playerWon)
+            {
+                // Presenter:
+                Console.WriteLine();
+                Console.WriteLine($"{playerTwoName}, you have {livesRemaining} lives left. Guess a letter from the secret word (in upper case)");
+
+                // Write their guess so far:
+                Console.WriteLine("So far, you've guessed:");
+                for (int i = 0; i < answerPreview.Length; i++)
+                {
+                    Console.Write(answerPreview[i] + " ");
+                }
+                Console.WriteLine();
+
+                // Store the guess
+                char guess = Console.ReadKey().KeyChar;
+
+                // If they're wrong, subtract a life
+                if (!Validator.IsGuessCorrect(secretWord, guess))
+                    livesRemaining--;
+
+                // If they're right, add letter to player2's construction of the word, in every position where that letter appears (this is displayed back to them at the start of the while loop)
+                for (int i = 0; i < secretWordInput.Length; i++)
+                {
+                    if (secretWord[i] == guess)
+                        answerPreview[i] = guess;
+                }
+
+                //if they've got the whole word, end the game
+                string completedAnswer = new string(answerPreview);
+
+                if (completedAnswer == secretWord)
+                    playerWon = true;
+            }
+
+            if (playerWon = true)
+            {
+                Console.WriteLine($"YOU WON! {playerTwoName}'s word was:");
+                for (int i = 0; i < answerPreview.Length; i++)
+                {
+                    Console.Write(answerPreview[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("GAME OVER!!! YOU RAN OUT OF LIVES!");
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Hangman/Validator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Hangman
+{
+    internal static class Validator
+    {
+        public static string GetValidSecretWord(string secretWordInput)
+        {
+            var input = secretWordInput;
+            var numberOfUniqueCharacters = (new HashSet<char>(input)).Count;
+            
+            while (input.Contains(" ") || !Regex.IsMatch(input, @"^[a-zA-Z]+$") || numberOfUniqueCharacters > 9)
+            {
+                Console.WriteLine("That's not valid.");
+                input = Console.ReadLine();
+                numberOfUniqueCharacters = (new HashSet<char>(input)).Count;
+            }
+
+            return input;
+        }
+
+        public static bool IsGuessCorrect(string secretWordInput, char guess)
+        {
+            Console.WriteLine();
+
+            if (!secretWordInput.Contains(guess)) 
+            {
+                Console.WriteLine("That letter isn't in the secret word!");
+                return false;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("CORRECT!");
+            return true;
+        }
+    }
+}
